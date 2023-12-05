c3db8ac12e1be233da0a3a9884371456dd55f7c5
Torstein Holmengen <torstein.holmengen@gmail.com>
2023-12-05 21:14:15 +0800

    Some simulation progress, still not maching qt

---
+++ simulations.py
@@ -0,0 +1,55 @@
+import random
+import simpy
+import numpy as np
+
+class MM1Queue:
+    def __init__(self, arrival_rate, service_rate, num_customers):
+        self.arrival_rate = arrival_rate
+        self.service_rate = service_rate
+        self.num_customers = num_customers
+        self.env = simpy.Environment()
+        self.queue = simpy.Store(self.env)
+        self.server = simpy.Resource(self.env, capacity=1)
+        self.queue_lengths = []
+        self.done = False
+        self.rng = np.random.default_rng()
+
+    def customer_arrival(self):
+        for i in range(self.num_customers):
+            yield self.env.timeout(self.rng.poisson(1/self.arrival_rate))
+            self.env.process(self.customer_service())
+        self.done = True
+
+    def customer_service(self):
+        with self.server.request() as request:
+            yield request
+            yield self.env.timeout(self.rng.exponential(1/self.service_rate))
+
+    def run_simulation(self):
+        self.env.process(self.customer_arrival())
+        self.env.process(self.stats_processing())
+        self.env.run()
+    
+    # def run_simulation(self, sim_time):
+    #     self.env.process(self.arrival_process())
+    #     self.env.run(until=sim_time)
+
+    def stats_processing(self):
+        while not self.done:
+            res = self.server
+            self.queue_lengths.append(len(res.queue))
+            # print(f'{res.count} of {res.capacity} slots are allocated.')
+            # print(f'  Users: {len(res.users)}')
+            # print(f'  Queued events: {len(res.queue)}')
+            yield self.env.timeout(0.1)
+        print("Simulation complete")
+        print(f'Average queue length: {np.mean(self.queue_lengths)}')
+
+# Example usage
+if __name__ == "__main__":
+    arrival_rate = 0.5  # average arrival rate of customers per unit of time
+    service_rate = 0.8  # average service rate of the server per unit of time
+    num_customers = 10000  # total number of customers to simulate
+
+    queue = MM1Queue(arrival_rate, service_rate, num_customers)
+    queue.run_simulation()
